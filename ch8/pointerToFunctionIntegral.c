2214092ea55119c99b7f183998f5b7beb1d16882
urmyfaith <urmyfaith@qq.com>
2014-10-28 22:03:28 +0800

    integral 积分算法：矩形下的面积

--- ch8/pointerToFunctionIntegral.c
+++ ch8/pointerToFunctionIntegral.c
@@ -15,13 +15,16 @@ int main()
     float integral(float low_limit,float upper_limit, float (*p)(float x));
     float func1(float x);
     float func2(float x);
+    float func3(float x);
+    float func4(float x);
+    float func5(float x);
     
     float low_limit,upper_limit,result;
 	int choice;
     printf("integral wich funciotn f(x)|(low_limit,upper_limit)\n");
     printf("input low_limit and upper_limit:\n");
     scanf("%f,%f",&low_limit,&upper_limit);
-    printf("choose function f(x)\n\t1-->f(x)=2*x\n\t2-->f(x)=2*x+1\n");
+    printf("choose function f(x)\n\t1-->f(x)=1+x\n\t2-->f(x)=2*x+1\n\t3-->f(x)=e^x+1\n\t4-->f(x)=(1+x)^2\n\t5-->f(x)=x^3\n");
 	scanf("%d",&choice);
 	switch(choice)
 	{
@@ -30,6 +33,16 @@ int main()
 			break;
 		case 2:
 			result=integral(low_limit,upper_limit,func2);
+			break;
+		case 3:
+			result=integral(low_limit,upper_limit,func3);
+			break;
+
+		case 4:
+			result=integral(low_limit,upper_limit,func4);
+			break;
+		case 5:
+			result=integral(low_limit,upper_limit,func5);
 			break;
 		default:
 			printf("please choice the right func.");
@@ -40,7 +53,7 @@ int main()
 float func1(float x)
 {
     float s;
-    s = 2*x ;
+    s = 1 + x ;
     return s;
 }
 
@@ -48,10 +61,28 @@ float func1(float x)
 float func2(float x)
 {
     float s;
-    s = 2*x + 1;
+    s = 2*x + 3;
     return s;
 }
 
+float func3(float x)
+{
+    float s;
+    s = exp(x) + 1;
+    return s;
+}
+float func4(float x)
+{
+    float s;
+    s = pow((1 + x ),2);
+    return s;
+}
+float func5(float x)
+{
+    float s;
+    s = pow(x,3);
+    return s;
+}
 float integral(float a,float b,float(*p)(float x))
 {
     float x,result=0;
@@ -61,7 +92,7 @@ float integral(float a,float b,float(*p)(float x))
         result = result + (*p)(x) * dx;
         x = x + dx;
     }
-    printf("%.f\n",result);
+    printf("%10.5f\n",result);
     return result;
 }
 
